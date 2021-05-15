e8dd0ec18fe00bcd6e2918be640d10e95412e271
Kiatkachorn Saripan <kiatkachorn@aslsecurities.com>
2021-05-16 02:20:12 +0700

    Revert "Change to dynamic setup file"
    This reverts commit af696b01ece476d003c36488d355b866310aa076.

--- setup.py
+++ setup.py
@@ -1,26 +1,3 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="data_manager_ksaripan",
-    version="0.0.1",
-    author="Kiatkachorn Saripan",
-    author_email="author@example.com",
-    description="A small example package",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/ksaripan/data_manager",
-    project_urls={
-        "Bug Tracker": "https://github.com/ksaripan/data_manager/issues",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
-)
+setuptools.setup()
